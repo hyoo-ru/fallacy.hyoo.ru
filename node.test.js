7e33c6266a68bbc20572d84c2f01c694fe703570
nin-jin <nin-jin@users.noreply.github.com>
2021-05-27 07:51:52 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -10625,6 +10625,7 @@ var $;
             return $.$mol_fail(new Error('dom_tree() not implemented'));
         }
     }
+    Symbol.toStringTag;
     $.$mol_jsx_view = $mol_jsx_view;
 })($ || ($ = {}));
 //view.js.map
