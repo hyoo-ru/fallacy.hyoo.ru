6ba42929fff6254f96021af7753b71fd2594dc09
nin-jin <sairi-na-tenshi@ya.ru>
2021-07-15 18:35:50 +0300

    Update fallacy.view.css.ts

--- fallacy.view.css.ts
+++ fallacy.view.css.ts
@@ -10,14 +10,16 @@ namespace $.$$ {
 			},
 		},
 
-		Filters: {
-			padding: rem(.75),
-		},
-
 		Content: {
+			
 			flex: {
 				basis: rem(60),
 			},
+			
+			Body: {
+				padding: rem(.75),
+			},
+
 		},
 
 		Cards_empty: {
