bc23f371ac450e8c8b30c63f8aaa649b8ef0edf9
nin-jin <nin-jin@users.noreply.github.com>
2023-10-06 07:12:26 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -7089,6 +7089,9 @@ var $;
                 ]);
                 el.selectionEnd = to;
                 el.selectionStart = from;
+                if (to !== from && el.selectionEnd === el.selectionStart) {
+                    el.selectionEnd = to;
+                }
             }
             selection_start() {
                 const el = this.dom_node();
