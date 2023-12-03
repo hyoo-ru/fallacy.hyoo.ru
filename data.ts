dc41a3301d002d71785019197bd39563def37b4b
Stefan Zerkalica <zerkalica@gmail.com>
2023-12-04 01:36:34 +0300

    $mol_view_tree2 refactor, https://github.com/hyoo-ru/mam_mol/pull/646

---
+++ data.ts
@@ -0,0 +1,12 @@
+namespace $ {
+	export type $hyoo_fallacy_tag = {
+		title: string
+	}
+
+	export type $hyoo_fallacy_data = {
+		tags: readonly string[]
+		title: string
+		descr: string
+		sample: string
+	}
+}
